c65689da947c2b1a72ba5e58d824af39bddd0074
YHChenAlice <95754616+YHChenAlice@users.noreply.github.com>
2022-06-08 10:21:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm.notebook import tqdm
+from google.cloud import bigquery
+from collections import defaultdict
+from datetime import datetime
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+
+df = pd.DataFrame(
+    np.random.randn(10, 5),
+    columns=("col %d" % i for i in range(5)))
+
+# Display a static table
+st.table(df)
+
+# Display an interactive table
+st.dataframe(df)
